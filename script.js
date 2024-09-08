dbc00e003ca5944fdfba49bb02b6b84126db8393
VeraTheDiver <61917@ldcsb.org>
2024-09-08 08:55:44 -0400

    lessn 29

---
+++ script.js
@@ -0,0 +1,36 @@
+let menu = document.querySelector (".fixed-menu")
+
+menu.addEventListener('click', function(event) {
+    let clickTarget = event.target;
+    let activeBtn = document.querySelector('.active')
+
+    if (clickTarget.classList.contains('nav-link')) {
+        activeBtn.classList.remove('active')
+        clickTarget.classList.add('active')
+    }
+})
+let classLink = '.main-link'
+window.onscroll = function(){
+    let h = document.documentElement.clientHeight
+    if (window.scrollY >= h*3.8) {
+        classLink = '.comments-link'
+    }
+    else if (window.scrollY >= h*2.8) {
+        classLink = '.works-link'
+    }
+    else if (window.scrollY >= h*1.8) {
+        classLink = '.skills-link'
+    }
+    else if (window.scrollY >= h*0.8) {
+        classLink = '.about-link'
+    }
+    else {
+        classLink = '.main-link'
+    }
+    let activeBtn = document.querySelector('.active')
+    let newActiveBtn = document.querySelector(classLink)
+    if (!newActiveBtn.classList.contains('active')) {
+        newActiveBtn.classList.add ('active')
+        activeBtn.classList.remove('active')
+    }
+}
